c4c50be0d50e0f1bfd9d7aefe332fc1311602966
Klajdo Qasolli <63808442+KlajdoQ@users.noreply.github.com>
2023-05-20 08:51:10 -0400

    destroy method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,6 @@
 class ArticlesController < ApplicationController 
-
+before_action :set_article, only:[:show, :edit, :update, :destroy]
     def show
-        @article = Article.find(params[:id])
     end
     
     def index 
@@ -23,12 +22,10 @@ class ArticlesController < ApplicationController
     end
 
     def edit 
-        @article = Article.find(params[:id])
     end
 
     def update 
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(article_params)
             flash[:notice] = "Article has been updated"
             redirect_to @article
         else 
@@ -36,8 +33,16 @@ class ArticlesController < ApplicationController
         end
     end
 
+    def destroy 
+        @article.destroy
+        redirect_to articles_path
+    end
 
     private
+
+    def set_article
+        @article=Article.find(params[:id])
+    end
 
     def article_params
         params.require(:article).permit(:title, :description)
