8ca5e549070f12a9d709e13e37d404bfd3442daa
Klajdo Qasolli <63808442+KlajdoQ@users.noreply.github.com>
2023-05-21 13:26:40 -0400

    created scaffold unit testing for categories

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,8 +1,34 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase 
+
+
+    def setup 
+        @category = Category.new(name: "Sports") #what is in setup runs first before the test so we can use it as a first step for each test
+    end
+
     test "category should be valid" do 
-        @category = Category.new(name: "Sports")
         assert @category.valid?
+    end
+
+    test "name shoud be present" do 
+        @category.name =" "
+        assert_not @category.valid?
+    end
+
+    test "name should be unique" do 
+        @category.save 
+        @category2 = Category.new(name: "Sports")
+        assert_not @category2.valid?
+    end
+
+    test "name should not be too long" do 
+        @category.name = "a" * 26
+        assert_not @category.valid?
+    end
+
+    test "name should not be too short" do 
+        @category.name = "aa" 
+        assert_not @category.valid?
     end
 end
